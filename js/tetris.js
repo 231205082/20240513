4510341d7960b2f2aba046a8da1c755bf8daf032
231205082 <146417149+231205082@users.noreply.github.com>
2024-05-13 15:59:42 +0900

    ピースの色を変更1
    白青

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'white', 'orange', 'white', 'yellow', 'red', 'lime', 'purple'
+    'skyblue', 'orange', 'white', 'yellow', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
